8140c1e951607f1fc4f7f4c80c3e275211db1585
Prof. G. Sandoval <gussand@gmail.com>
2021-11-16 11:15:33 -0500

    add completion to c code and working with timer

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# VSCodex
+# VSCodex-NYU
 
 Visual Studio Code extension to support the new codex API by OpenAI.
+This extension is based on some of the code for VSCodex by https://github.com/VincentHch
 
 An *API key* (access token) is required in order to use this extension. This token is issued by OpenAI and must be in an environment variable named `OPENAI_API_KEY`. You can alternatively specify the API key in the extension settings although this might be less secure, and is generally discouraged.
 
--- src/extension.ts
+++ src/extension.ts
@@ -13,16 +13,31 @@ const superagent = require("superagent");
 //Create output channel
 const vscodexOut = vscode.window.createOutputChannel("vscodex");
 
-async function appendCurrentSelection(context: vscode.ExtensionContext) {
+async function appendCurrentSelection(context: vscode.ExtensionContext) : Promise<string> {
 
-    let selectionObj = getSelection();
-    if (!selectionObj || !selectionObj.selection || !selectionObj.text)
-    {
-        vscode.window.showWarningMessage("No text selected! Please select some text.");
-        return;
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return '';
     }
-    let selection = selectionObj.selection;
-    let text = selectionObj.text;
+
+    const doc = editor.document;
+    const pos = editor.selection.active;
+    const text = doc.getText(new vscode.Range(new vscode.Position(0, 0), pos));
+
+    let suggestionText: string = ''; 
+
+    // let selectionObj = getSelection();
+    // if (!selectionObj || !selectionObj.selection || !selectionObj.text)
+    // {
+    //     vscode.window.showWarningMessage("No text selected! Please select some text.");
+    //     return;
+    // }
+    // let selection = selectionObj.selection;
+    // let text = selectionObj.text;
+
+    // Display a progress bar while fetching the response
+
 
     // Display a progress bar while fetching the response
 	vscode.window.withProgress({
@@ -47,12 +62,26 @@ async function appendCurrentSelection(context: vscode.ExtensionContext) {
                     return;
                 }
 
-                if (vscode.window.activeTextEditor) {
-                    vscode.window.activeTextEditor.edit(editBuilder => {
-                        editBuilder.insert(selection.end, completion);
-                    });
-            }});
+                // if (vscode.window.activeTextEditor) {
+                //     vscode.window.activeTextEditor.edit(editBuilder => {
+                //         editBuilder.insert(selection.end, completion);
+                //     });
+                // }
+
+                editor.edit((editBuilder) => {
+                    editBuilder.insert(pos, completion);
+                });
+
+                // const textCompletion = new vscode.CompletionItem("text");
+                // textCompletion.kind = vscode.CompletionItemKind.Text;
+                // textCompletion.insertText =  completion; 
+                // suggestionText = completion;
+
+        });
 	});
+    
+    return suggestionText;
+
 }
 
 /**
@@ -74,14 +103,74 @@ async function pickAndSetLevel(context: vscode.ExtensionContext) {
     }
 }
 
+// provider1 = async () => { }.languages.registerCompletionItemProvider('language:c', {
+//     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+//         const textCompletion = new vscode.CompletionItem("text");
+//         textCompletion.kind = vscode.CompletionItemKind.Text;
+//         let theText : string = await appendCurrentSelection(context);
+//         textCompletion.insertText =  theText;
+
+//         return [
+//             textCompletion,
+//         ];
+//     }
+// });
+
+
+
 // this method is called when the extension is activated
 // your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
+
+    let timer = undefined;
+    let output: vscode.CompletionItem[] | undefined = undefined;
+
+
+	const provider2 = vscode.languages.registerCompletionItemProvider('c', {
+
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+            // clearTimeout(timer);
+			// timer = setTimeout(function() {
+			// 	// a simple completion item which inserts `Hello World!`
+			// 	const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+			// 	output = [
+			// 		simpleCompletion,
+			// 	];
+
+			// }, 1000);
+            // a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which will be
+			// honored by the editor.
+
+            // a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+			const commitCharacterCompletion = new vscode.CompletionItem('console');
+			commitCharacterCompletion.commitCharacters = ['.'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+
+            output =[
+                snippetCompletion,
+                commitCharacterCompletion
+            ];
+
+			return output;
+		}
+	});
+    
 
 	const predict = vscode.commands.registerCommand("vscodex.predict", async function() {
 		appendCurrentSelection(context);
 	});
-	context.subscriptions.push(predict);
+
+    context.subscriptions.push(provider2);
 
     const setLevelAndPredict = vscode.commands.registerCommand("vscodex.setLevelAndPredict", async function() {
         await pickAndSetLevel(context);
